942baacd8188b7f33d32252d64d83426b87124d3
Mamadou S Diallo <me@msdiallo.io>
2024-02-04 16:10:09 +0000

    experimenting on gcp

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 # Import packages
 import os
 import dash
-from dash import Dash, html, dcc
-import dash_auth
+from dash import Dash, html, dcc, Input, Output, callback
 import dash_bootstrap_components as dbc
 
-# from script.datasets import datasets_fe, datasets_be
+# import dash_enterprise_auth as auth
 
-from env.secrets import VALID_USERNAME_PASSWORD_PAIRS
+# import dash_auth
+# from env.secrets import VALID_USERNAME_PASSWORD_PAIRS
 
 
 # Initialize the app
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.FLATLY])
+server = app.server
 
-auth = dash_auth.BasicAuth(app, VALID_USERNAME_PASSWORD_PAIRS)
+# auth = dash_auth.BasicAuth(server, VALID_USERNAME_PASSWORD_PAIRS)
+
 
 app.title = "FeeLoST"
 
@@ -35,9 +37,12 @@ app.layout = html.Div(
         dcc.Location(id="url", refresh=False),
         html.H4(
             "Feedback Loop System (FeeLoST)",
+            id='header-title',
             style={"textAlign": "center"},
             className="bg-primary text-white p-2 mb-2 text-center",
         ),
+        # html.Div(auth.create_logout_button(), className='two columns', style={'marginTop': 30}),
+        # html.Div(id="auth-input", style={"display": "none"}),
         nav_menu,
         dbc.Container(id="page-content"),
         dash.page_container,
@@ -45,9 +50,16 @@ app.layout = html.Div(
     # className="all",
 )
 
-app.config["VALID_USERNAME_PASSWORD_PAIRS"] = os.environ.get("VALID_USERNAME_PASSWORD_PAIRS")
+# @callback(Output("header-title", "children"), Input("url", "pathname"))
+# def update_title(_):
+
+#     # print user data to the logs
+#     print(auth.get_user_data())
+
+#     # update header with username
+#     return "Hello {}".format(auth.get_username())
 
 # Run the app
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=Truey, host="0.0.0.0", port="8080", use_reloader=False)
